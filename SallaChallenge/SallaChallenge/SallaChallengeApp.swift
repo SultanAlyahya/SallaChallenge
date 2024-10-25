3e5b9c2d892bd5e93c9e6fbdc42bb678e822159b
SultanAlyahya <sultan.alyahya@hotmail.com>
2024-10-25 22:06:05 +0300

    initiate prject

---
+++ SallaChallenge/SallaChallenge/SallaChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SallaChallengeApp.swift
+//  SallaChallenge
+//
+//  Created by Sultan alyahya on 22/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct SallaChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
