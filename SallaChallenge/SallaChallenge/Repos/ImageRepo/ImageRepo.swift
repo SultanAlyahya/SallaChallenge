8fdc67deadeeaed12a7f2512d97730fbebefa39d
SultanAlyahya <sultan.alyahya@hotmail.com>
2024-10-28 02:31:57 +0300

    implement image repo

--- SallaChallenge/SallaChallenge/Repos/ImageRepo/ImageRepo.swift
+++ SallaChallenge/SallaChallenge/Repos/ImageRepo/ImageRepo.swift
@@ -8,5 +8,11 @@
 import Foundation
 
 class ImageRepo {
+    @Injected(\.remoteDataSource) private var remoteDataSource
     
+    func getImage(path: String) async throws -> Data {
+        let url = URL(string: path)!
+        let data = try await remoteDataSource.makeRequst(url: url, headers: nil, body: nil)
+        return data
+    }
 }
